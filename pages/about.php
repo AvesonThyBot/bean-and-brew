260b891cf63264dec9b7500f5712966d0f8ea18b
Aveson <avesoncarvalho25@gmail.com>
2024-01-20 15:23:31 +0000

    added all the exisiting files to repo

---
+++ pages/about.php
@@ -0,0 +1,11 @@
+<?php // Navbar/ Header
+$title = "About - Bean and Brew";
+$currentActive = "about";
+include_once("../includes/header.inc.php")
+?>
+
+
+<?php
+// Footer 
+include_once("../includes/footer.inc.php")
+?>
---
+++ pages/book.php
@@ -0,0 +1,11 @@
+<?php // Navbar/ Header
+$title = "Book A Space - Bean and Brew";
+$currentActive = "book";
+include_once("../includes/header.inc.php")
+?>
+
+
+<?php
+// Footer 
+include_once("../includes/footer.inc.php")
+?>
